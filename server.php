53021ca3c5b92e716cff760de7698218cb2c2873
chenyanming <chenym434@mingchao.com>
2017-08-28 15:53:31 +0800

    [dev] add redis

--- server.php
+++ server.php
@@ -1,24 +1,29 @@
 <?php
 
+require_once "RedisDB.php";
+
 //创建websocket服务器对象，监听0.0.0.0:9502端口
-$ws = new swoole_websocket_server("0.0.0.0", 9502);
+$ws = new swoole_websocket_server("0.0.0.0", 29501);
 
 //监听WebSocket连接打开事件
 $ws->on('open', function ($ws, $request) {
-    $fd[] = $request->fd;
-    $GLOBALS['fd'][] = $fd;
-    //$ws->push($request->fd, "hello, welcome\n");
+    global $redis;
+    $redis->lpush("fd", $request->fd);
+
+    $arList = $redis->lrange("fd",0,5);
+    print_r($arList);
+
+//    var_dump($request);
+    $ws->push($request->fd, "hello, welcome\n");
 });
 
 //监听WebSocket消息事件
 $ws->on('message', function ($ws, $frame) {
     $msg =  'from'.$frame->fd.":{$frame->data}\n";
-//var_dump($GLOBALS['fd']);
+var_dump($GLOBALS['fds']);
 //exit;
-    foreach($GLOBALS['fd'] as $aa){
-        
-            $ws->push($aa[0],$msg);
-        
+    foreach($GLOBALS['fds'] as $aa){
+        $ws->push($aa,$msg);
     }
    // $ws->push($frame->fd, "server: {$frame->data}");
     // $ws->push($frame->fd, "server: {$frame->data}");
@@ -26,6 +31,9 @@ $ws->on('message', function ($ws, $frame) {
 
 //监听WebSocket连接关闭事件
 $ws->on('close', function ($ws, $fd) {
+    $tempArr = array_flip($GLOBALS['fds']);
+    unset($tempArr[$fd]);
+    $GLOBALS['fds'] = array_flip($tempArr);
     echo "client-{$fd} is closed\n";
 });
 
